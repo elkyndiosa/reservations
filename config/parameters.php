4074bf874b1534bde54a4a24a6f878f106949e62
Gustavo Diosa <elkyndiosa23@gmail.com>
2020-02-01 10:13:47 -0500

    Update and rename config/parameters.php to  config/parameters.php

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
-define('base_url', 'http://localhost/ReserverManager/');
+define('base_url', 'https://reservationsrest.herokuapp.com/');
 define('controller_default', 'reservationController');
 define('action_default', 'index');
 date_default_timezone_set("America/Bogota");
