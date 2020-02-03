172be928b75748f79a78001f9899b4f697b80922
elkyndiosa <elkyndiosa23@gmail.com>
2020-02-03 16:26:29 -0500

    new commit

--- config/db.php
+++ config/db.php
@@ -1,9 +1,19 @@
 
 <?php 
-
 class database{
     public static function connect(){
-        $db = new mysqli('localhost', 'root', '', 'reservation');
+        $db = new mysqli('160.153.129.220', 'gustavo', 'vieja321', 'reservas');
         $db-> query("SET NAMES 'utf8'");
         return $db;
     }
+}
+
+
+
+
+//class database{
+//    public static function connect(){
+//        $db = new mysqli('localhost', 'root', '', 'reservation');
+//        $db-> query("SET NAMES 'utf8'");
+//        return $db;
+//    }
--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,10 @@
 <?php
-define('base_url', 'http://localhost/reservermanager/');
-define('controller_default', 'reservationController');
-define('action_default', 'index');
-date_default_timezone_set("America/Bogota");
+define ( 'base_url' , 'https://reservationsrest.herokuapp.com/' );
+define ( 'controller_default' , 'reserveController' );
+define ( 'action_default' , 'index' );
+date_default_timezone_set ( "América / Bogotá" );
+
+//define('base_url', 'http://localhost/reservermanager/');
+//define('controller_default', 'reservationController');
+//define('action_default', 'index');
+//date_default_timezone_set("America/Bogota");
