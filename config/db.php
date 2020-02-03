41b5b838bd7a6aeae9ae4eadd0147bb7a5fb3052
elkyndiosa <elkyndiosa23@gmail.com>
2020-02-03 17:03:45 -0500

    new

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 <?php 
 class database{
     public static function connect(){
-        $db = new mysqli('160.153.129.220', 'gustavo', 'vieja321', 'reservas');
+        $db = new mysqli('160.153.153.136', 'gustavo', 'vieja321', 'reservas');
         $db-> query("SET NAMES 'utf8'");
         return $db;
     }
