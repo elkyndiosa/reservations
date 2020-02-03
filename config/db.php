5e4993c8d0b887a8b33e305d93ab035e1ed9b8cc
elkyndiosa <elkyndiosa23@gmail.com>
2020-02-03 16:16:52 -0500

    New commit actualization

--- config/db.php
+++ config/db.php
@@ -1,18 +1,9 @@
 
 <?php 
-//-----------DATOS PARA SERVIDOR LOCAL----------------------
-//class database{
-//    public static function connect(){
-//        $db = new mysqli('localhost', 'root', '', 'reservation');
-//        $db-> query("SET NAMES 'utf8'");
-//        return $db;
-//    }
-//-----------DATOS PARA SERVIDOR WEB----------------------
 
-class database{ 
+class database{
     public static function connect(){
-        $db = new mysqli ('160.153.129.220', 'gustavo', 'vieja321','reservas');
+        $db = new mysqli('localhost', 'root', '', 'reservation');
         $db-> query("SET NAMES 'utf8'");
         return $db;
     }
-}
--- config/parameters.php
+++ config/parameters.php
@@ -1,14 +1,5 @@
 <?php
-//-----------DATOS PARA SERVIDOR LOCAL----------------------
-
-//define('base_url', 'http://localhost/reservermanager/');
-//define('controller_default', 'reservationController');
-//define('action_default', 'index');
-//date_default_timezone_set("America/Bogota");
-//
-//-----------DATOS PARA SERVIDOR WEB----------------------
-
-define( 'base_url', 'https://reservationsrest.herokuapp.com/');
-define( 'controller_default', 'reserveController' );
-define( 'action_default', 'index');
-date_default_timezone_set ( "América / Bogotá" );
+define('base_url', 'http://localhost/reservermanager/');
+define('controller_default', 'reservationController');
+define('action_default', 'index');
+date_default_timezone_set("America/Bogota");
