b94a82baedfc96b98a30a3b747fb1b568dbff834
Gustavo Diosa <elkyndiosa23@gmail.com>
2020-02-04 01:50:06 -0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 class database{
     public static function connect(){
-        $db = new mysqli('160.153.129.220', 'gustavo', '1234', 'reservas');
+        $db = new mysqli('160.153.129.220', 'gustavo', '1234', 'reservations');
         $db-> query("SET NAMES 'utf8'");
         return $db;
     }
