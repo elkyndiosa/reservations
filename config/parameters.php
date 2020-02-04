726d9ed43fb55d2828260c9930007bafd92cd7c5
Gustavo Diosa <elkyndiosa23@gmail.com>
2020-02-04 01:51:54 -0500

    Update parameters.php

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <? php
-define ( 'base_url' , 'https://reservationslive.herokuapp.com/' );
+define ( 'base_url', 'https://reservationslive.herokuapp.com/' );
 define ( 'controller_default' , 'reserveController' );
 define ( 'action_default' , 'index' );
 date_default_timezone_set ( "América / Bogotá" );
